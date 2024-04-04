b3f7b8bf49656dbc6a2fb010a50da6e2e340057c
ryanalanyee <143042293+ryanalanyee@users.noreply.github.com>
2024-04-04 11:42:06 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -13,7 +13,7 @@ public class WordList
    int count = 0;
     for(int i = 0; i < myList.size(); i++){
         if(myList.get(i).length() == len)
-          count++
+          count++;
     }
     return count;
   }
